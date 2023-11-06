5096586558cfb29b94b1fcccd85806b5e0a4d1bb
helloRija <149434811+helloRija@users.noreply.github.com>
2023-11-06 13:57:19 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Rija Rochfort.", "UX Designer.", "Visual Designer.", "Contractor.", "UX Strategist"],
       typeSpeed: 100,
       loop: true,
     });
